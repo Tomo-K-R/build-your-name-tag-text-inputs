5ea039773a7a10deda365e66e58d6aff2bc5e49f
Tomo-K-R <tomomikojo@hotmail.com>
2022-05-20 12:29:08 +0100

    first commit

---
+++ build/precache-manifest.bf8a2c34e01d5fc77ea116d763fd86fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aaa3c516748e32d05f1",
+    "url": "/build-your-name-tag-text-inputs/static/css/main.b95460f9.chunk.css"
+  },
+  {
+    "revision": "9aaa3c516748e32d05f1",
+    "url": "/build-your-name-tag-text-inputs/static/js/main.9aaa3c51.chunk.js"
+  },
+  {
+    "revision": "d59a03719dd73308f1c2",
+    "url": "/build-your-name-tag-text-inputs/static/js/1.d59a0371.chunk.js"
+  },
+  {
+    "revision": "71f3008ef0b16c27887f",
+    "url": "/build-your-name-tag-text-inputs/static/js/runtime~main.71f3008e.js"
+  },
+  {
+    "revision": "8c65717c8401e92346117fe365d635e2",
+    "url": "/build-your-name-tag-text-inputs/index.html"
+  }
+];
